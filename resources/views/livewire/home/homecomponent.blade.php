562ba1a7baa6263b569c8b1ad16176418ff7999b
Pedro Losqui <54372473+pedro-losqui@users.noreply.github.com>
2021-11-10 15:27:01 -0300

    Update opções de filtro select

--- resources/views/livewire/home/homecomponent.blade.php
+++ resources/views/livewire/home/homecomponent.blade.php
@@ -55,12 +55,13 @@
                                 </div>
                                 <select class="form-control" wire:model="type" id="example-select"
                                     name="example-select">
-                                    <option value="">Todos</option>
-                                    <option value="1">Admissional</option>
                                     <option value="2">Periódico</option>
-                                    <option value="3">Mudança de função</option>
-                                    <option value="3">Retorno ao trabalho</option>
-                                    <option value="3">Demissional</option>
+                                    <option value="1">Admissional</option>
+                                    <option value="3">Mudança de Função</option>
+                                    <option value="4">Demissional</option>
+                                    <option value="5">Monitoração Pontual/Consulta</option>
+                                    <option value="6">Complementares</option>
+                                    <option value="7">Retorno ao Trabalho</option>
                                 </select>
                             </div>
                         </div>
