6eb9ef56e1c046bf99995237188a568c0d09477f
Pedro Losqui <54372473+pedro-losqui@users.noreply.github.com>
2021-12-03 17:32:22 -0300

    Update create

---
+++ app/Http/Livewire/Report/Reportcomponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Report;
+
+use Livewire\Component;
+
+class Reportcomponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.report.reportcomponent');
+    }
+}
