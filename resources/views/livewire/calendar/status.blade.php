79756367c8b67baeb236ca85f28b494d44f18f0d
Pedro Losqui <54372473+pedro-losqui@users.noreply.github.com>
2021-12-16 14:39:27 -0300

    Update tela de status

--- resources/views/livewire/calendar/status.blade.php
+++ resources/views/livewire/calendar/status.blade.php
@@ -1,11 +1,10 @@
-@if($employee)
     <div wire:ignore.self class="modal fade" id="employeeStatus" tabindex="-1" role="dialog"
         aria-labelledby="modal-fadein" aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
-                <div class="block mb-0 block-themed block-transparent" wire:loading.remove>
+                <div class="block mb-0 block-themed block-transparent">
                     <div class="block-header bg-primary-dark">
-                        <h3 class="block-title">Solicitação {{ $employee->datSol }}</h3>
+                        <h3 class="block-title">Status</h3>
                         <div class="block-options">
                             <button type="button" class="btn-block-option" data-dismiss="modal" aria-label="Close">
                                 <i class="si si-close"></i>
@@ -21,7 +20,7 @@
                         {{ $minutos }}
                     </div>
                 </div>
-                <div class="modal-footer" wire:loading.remove>
+                <div class="modal-footer">
                     <button type="button" wire:click='recordStatus' class="btn btn-alt-success">Atualizar</button>
                     <button type="button" class="btn btn-alt-secondary" data-dismiss="modal">Fechar</button>
                 </div>
@@ -37,4 +36,3 @@
             </div>
         </div>
     </div>
-@endif
