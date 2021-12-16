2d0d951fbb65c87c76c33e59c48cc6561ab150f6
Pedro Losqui <54372473+pedro-losqui@users.noreply.github.com>
2021-12-16 15:31:23 -0300

    Data de requisição atualizada

--- app/Repository/SoapClient.php
+++ app/Repository/SoapClient.php
@@ -13,10 +13,8 @@ class SoapClient
     public function __construct(SoapWrapper $soapWrapper)
     {
 
-      $this->from = date('16/11/2021');
-      $this->to = date('17/11/2021');
-    //   $this->from = date('d/m/Y', strtotime('-10 day'));
-    //   $this->to = date('d/m/Y', strtotime('+15 day'));
+      $this->from = date('d/m/Y', strtotime('-10 day'));
+      $this->to = date('d/m/Y', strtotime('+15 day'));
 
       $this->soapWrapper = $soapWrapper;
 
